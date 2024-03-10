0940b91402142d5c28509af366d4ab3444b77571
Anwar Sh. <alvaro.sh03@gmail.com>
2024-03-10 21:54:05 +0500

    Add logging of requests

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,12 +11,16 @@ import Database.PostgreSQL.Simple
 import Db.Operations
 import Network.Wai
 import Network.Wai.Handler.Warp
+import Network.Wai.Logger (withStdoutLogger)
 import Servant
 
 startApp :: IO ()
 startApp = do
   conn <- connectToDb
-  run 8080 $ app conn
+
+  withStdoutLogger $ \aplogger -> do
+    let settings = setPort 8080 $ setLogger aplogger defaultSettings
+    runSettings settings (app conn)
 
 app :: Connection -> Application
 app = serve api . server
